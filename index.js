7a404c34d519b3a1e73e01ff1a6108286eb785ea
sreym <sergeyreym@gmail.com>
2018-08-10 17:57:40 +0800

    sample anim

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+// http://svgjs.com/animating/
 let draw;
 
 async function loadFile(draw, file){
@@ -9,6 +10,10 @@ async function loadFile(draw, file){
 async function main(){
   draw = SVG('drawing').size(500, 500);
   await loadFile(draw, 'drawing.svg');
+  let rect = draw.select('#rect1649').get(0);
+  let r = {width: rect.width(), height: rect.height()};
+  rect.mouseover(()=>rect.animate(300, '<', 0).size(r.width*1.1, r.height*1.1));
+  rect.mouseout(()=>rect.animate(100, '<', 0).size(r.width, r.height)); 
 }
 
 document.addEventListener('DOMContentLoaded', main);
