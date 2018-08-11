9a2f99b421f95313dc37346274e6d2eeb22ba757
sreym <sergeyreym@gmail.com>
2018-08-11 11:52:10 +0800

    add anim

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+// https://inkscape.org/ru/
+// https://github.com/sreym/svg-simple
 // http://svgjs.com/animating/
 let draw;
 
@@ -10,10 +12,12 @@ async function loadFile(draw, file){
 async function main(){
   draw = SVG('drawing').size(500, 500);
   await loadFile(draw, 'drawing.svg');
-  let rect = draw.select('#rect1649').get(0);
+  let rect;
+  rect = draw.select('#rect1649').get(0);
   let r = {width: rect.width(), height: rect.height()};
-  rect.mouseover(()=>rect.animate(300, '<', 0).size(r.width*1.1, r.height*1.1));
-  rect.mouseout(()=>rect.animate(100, '<', 0).size(r.width, r.height)); 
+  rect.mouseover(()=>rect.animate(1000, '<>', 0).size(r.width*1.1, r.height*1.1));
+  rect.mouseout(()=>rect.animate(100, '<', 500).size(r.width, r.height)); 
+  rect.click(()=>rect.animate().attr({fill: '#00ff00'}));
 }
 
 document.addEventListener('DOMContentLoaded', main);
